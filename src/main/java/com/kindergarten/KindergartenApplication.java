70b90fd389079a5cbb7c802132043859e114c789
HuangZhiWen <215063628@qq.com>
2020-08-03 09:28:02 +0800

    黄智玟-更新09点27分

--- src/main/java/com/kindergarten/KindergartenApplication.java
+++ src/main/java/com/kindergarten/KindergartenApplication.java
@@ -1,5 +1,6 @@
 package com.kindergarten;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class KindergartenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KindergartenApplication.class, args);
+
+
     }
 
 }
--- src/main/java/com/kindergarten/service/ParentService.java
+++ src/main/java/com/kindergarten/service/ParentService.java
@@ -13,9 +13,10 @@ public interface ParentService {
     public LayuiData<CampusInfo> CampusInfo(int kinderId,int curPage, int pageSize);
     public LayuiData<PlatformInfo> PlatformInfo(int curPage, int pageSize);
     public List<Students> studentsList(int parentId);
-    public  LayuiData<Examination> examination(int studentId);
-    public  LayuiData<Monitor> monitors(int studentId);
+    public  LayuiData<Examination> examination(int studentId,int curPage, int pageSize);
+    public  LayuiData<Monitor> monitors(int studentId,int curPage, int pageSize);
     public PageBean<Meal> meals(int studentId,int curPage, int pageSize);
+    public LayuiData<Workrelease> studentWork(int studentId,int curPage, int pageSize);
 
 
 }
